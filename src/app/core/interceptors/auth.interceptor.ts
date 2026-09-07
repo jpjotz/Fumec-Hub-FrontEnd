309906072ea96764c9b166a8c71dbb4a1ddd4840
João Pedro <srdoog1@gmail.com>
2026-09-06 22:50:03 -0300

    fix: rota de refresh token sendo chamada no login

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,28 +1,23 @@
-import { HttpInterceptorFn } from "@angular/common/http";
-import { inject } from "@angular/core";
-import { catchError, switchMap } from "rxjs";
-import { AuthService } from "../services/auth.service";
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, switchMap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
   const authService = inject(AuthService);
 
   return next(req).pipe(
-
     catchError((error) => {
-
-      if (error.status === 401) {
-
-        return authService.refresh().pipe(
-          switchMap(() => next(req))
-        );
-
+      if (
+        error.status === 401 &&
+        !req.url.includes('/auth/login') &&
+        !req.url.includes('/auth/refresh') &&
+        !req.url.includes('/users/register')
+      ) {
+        return authService.refresh().pipe(switchMap(() => next(req)));
       }
 
       throw error;
-
-    })
-
+    }),
   );
-
-}
+};
