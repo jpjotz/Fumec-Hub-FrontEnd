8924f1941103edc603e2164d73c258e3d42efc75
João Pedro <srdoog1@gmail.com>
2026-09-11 18:22:31 -0300

    espero q resolva isso

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -5,16 +5,15 @@ import { tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
   constructor (private http: HttpClient) {}
 
-  private apiUrl = 'https://fumec-hub-backend.onrender.com/';
+  private apiUrl = '/api/';
 
   user = signal<any>(null);
 
   getMe() {
-    return this.http.get(this.apiUrl + 'users/me', {withCredentials: true}).pipe(
+    return this.http.get(this.apiUrl + 'users/me', { withCredentials: true }).pipe(
       tap((user) => {
         this.user.set(user);
       })
@@ -22,18 +21,18 @@ export class AuthService {
   }
 
   register(nome: string, email: string, password: string) {
-    return this.http.post(this.apiUrl + 'users/register', {name: nome, email, password}, {withCredentials: true});
+    return this.http.post(this.apiUrl + 'users/register', { name: nome, email, password }, { withCredentials: true });
   }
 
   login(email: string, password: string) {
-    return this.http.post(this.apiUrl + 'auth/login', {email, password}, {withCredentials: true});
+    return this.http.post(this.apiUrl + 'auth/login', { email, password }, { withCredentials: true });
   }
 
   refresh() {
-    return this.http.post(this.apiUrl + 'auth/refresh', {}, {withCredentials: true})
+    return this.http.post(this.apiUrl + 'auth/refresh', {}, { withCredentials: true });
   }
 
   logout() {
-    return this.http.post(this.apiUrl + 'auth/logout', {}, {withCredentials: true})
+    return this.http.post(this.apiUrl + 'auth/logout', {}, { withCredentials: true });
   }
 }
