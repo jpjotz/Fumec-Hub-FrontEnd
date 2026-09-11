967cbd8baed799280bfdb9f612bc9db43ee2e799
João Pedro <srdoog1@gmail.com>
2026-09-11 15:16:44 -0300

    fix

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,10 +1,9 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
-import { map, catchError, of } from 'rxjs';
+import { map, catchError, of, switchMap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
-
   const authService = inject(AuthService);
   const router = inject(Router);
 
@@ -12,8 +11,14 @@ export const authGuard: CanActivateFn = (route, state) => {
     map(() => true),
 
     catchError(() => {
-      router.navigate(['/']);
-      return of(false);
-    })
+      return authService.refresh().pipe(
+        switchMap(() => authService.getMe()),
+        map(() => true),
+        catchError(() => {
+          router.navigate(['/']);
+          return of(false);
+        }),
+      );
+    }),
   );
 };
